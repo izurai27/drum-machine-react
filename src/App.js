fa92c4a647378c8ed9c5c3a8736fc5443f71a169
Izma Raifani <izurai@gmail.com>
2022-01-20 10:56:25 +0700

    add keypress in app.js

--- src/App.js
+++ src/App.js
@@ -17,33 +17,51 @@ class App extends React.Component{
         {id:7,letter:'Z',sound:'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'} , 
         {id:8,letter:'X',sound:'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'},
         {id:9,letter:'C',sound:'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'} 
-      ]
+      ],
+      display:''
     }
     this.handleClick=this.handleClick.bind(this);
+    this.handleKey=this.handleKey.bind(this);
   }
 
   handleClick(event){
     // event.target.children.play();
     document.getElementById(event.target.innerText).play();
+    this.setState({
+      display:event.target.id
+    })
   
   }
 
   handleKey(event){
     console.log(event.key)
   }
+
+  componentDidMount() {
+    document.addEventListener('keydown', function(event){
+      console.log(event.key.toUpperCase(),document.getElementById(event.key.toUpperCase()));
+      document.getElementById(event.key.toUpperCase()).play();
+      
+      // this.setState({
+      //   display:event.key
+      // })
+    })
+  }
   
   render(){
     
     return (
       <div id="drum-machine">
-        <div id="display"></div>
-        {this.state.keyboard.map( keyb => 
-        <button key={keyb.id} className="drum-pad" onClick={this.handleClick} onKeyPress={this.handleKey}> {keyb.letter}
-          <audio className='clip' id={keyb.letter} >
-              <source src={keyb.sound} type="audio/mpeg"/>
-          </audio>
-        </button> )
-        }
+        <div id="display">{this.state.display}</div>
+        <br/>
+        <div className='container'>
+          {this.state.keyboard.map( keyb => 
+            <button id={'button'+keyb.letter} key={keyb.id} className="drum-pad" onClick={this.handleClick}> {keyb.letter}
+              <audio className='clip' id={keyb.letter} src={keyb.sound} type="audio/mpeg"/>
+              
+            </button> )
+          }
+        </div>
       </div>  
     )
 
